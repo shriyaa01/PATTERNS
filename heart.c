8966b6037e6f2002a17ffeaadde38bd82a480276
Shriya Srivastava <100456530+shriyaa01@users.noreply.github.com>
2022-06-01 10:19:23 +0530

    Create heart.c

---
+++ heart.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+   int i,j,n;
+   printf("ENTER VALUE OF row    -->");
+   scanf("%d",&n);
+   for(i=n/2; i<=n; i+=2)
+   {
+       for(j=1; j<n-i; j+=2)
+            printf(" ");
+        for(j=1; j<=i; j++)
+            printf("*");
+        for(j=1; j<=n-i; j++)
+             printf(" ");
+        for(j=1; j<=i; j++)
+             printf("*");
+        printf("\n");          
+   }\
+
+  for(int i=n; i>=1; i--)
+   {
+       for(j=i; j<n; j++)
+           printf(" ");
+       for(j=1; j<=(i*2)-1; j++)
+            printf("*");
+        printf("\n");      
+   }
+   return 0;
+   
+}
